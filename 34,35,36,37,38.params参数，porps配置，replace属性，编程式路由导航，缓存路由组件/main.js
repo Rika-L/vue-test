0f6560bb29fcb39a0c5c0674b7ad2a15ab3f3513
Rika-L <2209393334@qq.com>
2024-02-06 23:01:30 +0800

    Vue Day14

---
+++ 34,35,36,37,38.params参数，porps配置，replace属性，编程式路由导航，缓存路由组件/main.js
@@ -0,0 +1,21 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+
+//引入VueRouter
+import VueRouter from "vue-router";
+import router from "@/router";
+
+Vue.use(VueRouter)
+
+
+//关闭Vue的生产提示
+Vue.config.productionTip = false
+
+//创建vm
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router,
+})
---
+++ 34,35,36,37,38.params参数，porps配置，replace属性，编程式路由导航，缓存路由组件/router/index.js
@@ -0,0 +1,56 @@
+//该文件专门用于创建路由器
+import VueRouter from "vue-router";
+import About from "@/pages/About.vue";
+import Home from "@/pages/Home.vue";
+import News from "@/pages/News.vue";
+import Message from "@/pages/Message.vue";
+import Detail from "@/pages/Detail.vue";
+
+//创建一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About,
+        },
+        {
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: 'news',
+                    component: News,
+                },
+                {
+                    path: 'message',
+                    component: Message,
+                    children: [
+                        {
+                            name: 'xiangqing',
+                            path: 'detail',
+                            component: Detail,
+
+                            //第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给组件
+                            // props: {
+                            //     a: 1,
+                            //     b: 11,
+                            // }
+
+                            //第二种写法，值为布尔值，若布尔值为真，就会把该路由组件收到的所有params参数
+                            //以props形式传给组件
+                            // props: true,
+
+                            //第三种写法，值为函数
+                            props($route){
+                                return {
+                                    id: $route.query.id,
+                                    title: $route.query.title,
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+    ]
+})
